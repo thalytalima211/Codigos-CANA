cac0d89a8a9d92a9233ef98c98646e6ae1d64747
Heikey <joaofialho123123@gmail.com>
2025-08-03 16:17:32 -0300

    Questao 04: Format and finalize the code

--- questao4.cpp
+++ questao4.cpp
@@ -1,125 +1,97 @@
-#include <vector>
 #include <cstdio>
+#include <vector>
 
-template <typename T>
-class Arvore
-{
+template <typename T> class Arvore {
 public:
-    T valor;
-    Arvore *esquerda;
-    Arvore *direita;
+  T valor;
+  Arvore *esquerda;
+  Arvore *direita;
 
-    Arvore<T>(T valor)
-    {
-        this->valor = valor;
-        this->esquerda = nullptr;
-        this->direita = nullptr;
+  Arvore<T>(T valor) {
+    this->valor = valor;
+    this->esquerda = nullptr;
+    this->direita = nullptr;
+  }
+
+  Arvore *push(T valor) {
+    // Valor repetido
+    if (this->valor == valor)
+      return nullptr;
+
+    if (valor < this->valor) {
+      if (this->esquerda == nullptr) {
+        return this->esquerda = new Arvore(valor);
+      } else {
+        return this->esquerda->push(valor);
+      }
+    } else {
+      if (this->direita == nullptr) {
+        return this->direita = new Arvore(valor);
+      } else {
+        return this->direita->push(valor);
+      }
     }
+    return nullptr;
+  }
 
-    Arvore *push(T valor)
-    {
-        // Valor repetido
-        if (this->valor == valor)
-            return nullptr;
+  std::vector<T> getNosFolha() {
+    static auto out = std::vector<T>();
+    out.clear();
+    getNosFolhaHelper(&out, this);
+    return out;
+  }
 
-        if (valor < this->valor)
-        {
-            if (this->esquerda == nullptr)
-            {
-                return this->esquerda = new Arvore(valor);
-            }
-            else
-            {
-                return this->esquerda->push(valor);
-            }
-        }
-        else
-        {
-            if (this->direita == nullptr)
-            {
-                return this->direita = new Arvore(valor);
-            }
-            else
-            {
-                return this->direita->push(valor);
-            }
-        }
-        return nullptr;
+  void getNosFolhaHelper(std::vector<T> *out, Arvore<T> *node) {
+    if (node->esquerda != nullptr) {
+      getNosFolhaHelper(out, node->esquerda);
     }
-
-    std::vector<T> getNosFolha()
-    {
-        static auto out = std::vector<T>();
-        out.clear();
-        getNosFolhaHelper(&out, this);
-        return out;
+    out->push_back(node->valor);
+    if (node->direita != nullptr) {
+      getNosFolhaHelper(out, node->direita);
     }
+  };
 
-    void getNosFolhaHelper(std::vector<T> *out, Arvore<T> *node)
-    {
-        if (node->esquerda != nullptr)
-        {
-            getNosFolhaHelper(out, node->esquerda);
-        }
-        out->push_back(node->valor);
-        if (node->direita != nullptr)
-        {
-            getNosFolhaHelper(out, node->direita);
-        }
-    };
+  struct FindResult {
+    Arvore *node;
+    int nivel;
+  };
 
-    struct FindResult
-    {
-        Arvore *node;
-        int nivel;
-    };
+  FindResult find(T valor, int nivel = 0) {
+    if (valor == this->valor)
+      return FindResult{this, nivel};
+    if (valor < this->valor)
+      return this->esquerda->find(valor, nivel + 1);
+    if (valor > this->valor)
+      return this->direita->find(valor, nivel + 1);
+    return FindResult{nullptr, 0};
+  }
 
-    FindResult find(T valor, int nivel = 0)
-    {
-        if (valor == this->valor)
-            return FindResult{this, nivel};
-        if (valor < this->valor)
-            return this->esquerda->find(valor, nivel + 1);
-        if (valor > this->valor)
-            return this->direita->find(valor, nivel + 1);
-        return FindResult{nullptr, 0};
-    }
+  int getGrau(T valor) {
+    auto find = this->find(valor).node;
+    return (find->esquerda != nullptr) + (find->direita != nullptr);
+  };
 
-    int getGrau(T valor)
-    {
-        auto find = this->find(valor).node;
-        return (find->esquerda != nullptr) + (find->direita != nullptr);
-    }
+  int getProfundidade(T valor) { return this->find(valor).nivel; }
 
-    int getProfundidade(T valor)
-    {
-        return this->find(valor).nivel;
-    }
+  int getAltura() { return getAlturaHelper(this); }
 
-    int getAltura()
-    {
-        return getAlturaHelper(this);
-    }
-
-    int getAlturaHelper(Arvore *node)
-    {
-        if (node == nullptr)
-            return -1; // altura de árvore vazia é -1
-        int alturaEsquerda = getAlturaHelper(node->esquerda);
-        int alturaDireita = getAlturaHelper(node->direita);
-        return 1 + (alturaEsquerda > alturaDireita ? alturaEsquerda : alturaDireita);
-    }
+  int getAlturaHelper(Arvore *node) {
+    if (node == nullptr)
+      return -1; // altura de árvore vazia é -1
+    int alturaEsquerda = getAlturaHelper(node->esquerda);
+    int alturaDireita = getAlturaHelper(node->direita);
+    return 1 +
+           (alturaEsquerda > alturaDireita ? alturaEsquerda : alturaDireita);
+  }
 };
 
-int main()
-{
-    auto raiz = Arvore(10);
+int main() {
+  auto raiz = Arvore(10);
 
-    auto arr = {14, 18, 4, 9, 7, 15, 3, 4, 20, 9, 5};
-    for (auto &elem : arr)
-    {
-        auto node = raiz.push(elem);
-        if (node == nullptr)
-            printf("O valor %d é repetido e já está na árvore.");
-    }
+  auto arr = {14, 18, 4, 9, 7, 15, 3, 4, 20, 9, 5};
+  for (auto &elem : arr) {
+    auto node = raiz.push(elem);
+    if (node == nullptr)
+      printf("O valor %d é repetido e já está na árvore.\n", elem);
+  }
 }
